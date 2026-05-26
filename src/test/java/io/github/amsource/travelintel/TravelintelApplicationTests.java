b46734d00cfaab1b5f48a76e28ac3a8daa2a652e
am-source <a.mardini@gmx.net>
2026-05-26 21:11:34 +0200

    Init project, added spring boot config

---
+++ src/test/java/io/github/amsource/travelintel/TravelintelApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.amsource.travelintel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelintelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
